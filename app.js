30f6eae10606e2d25f5c80cd8eff6000823c7c87
Ryan <rduffy16@qub.ac.uk>
2019-06-19 21:54:35 +0100

    Finalised - Google Login and full authentication in place

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 
 //require and install the below package for the configuration of environment variables
 require('dotenv').config();
+//const md5 = require('md5');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-var encrypt = require('mongoose-encryption');
+const encrypt = require('mongoose-encryption');
+const bcrypt = require("bcrypt");
+const saltRounds =10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
-const app = express()
+const app = express();
 const port = 3000;
 
 // See below how we can access environment variables i.e.
@@ -21,28 +29,86 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+//continue here
+app.use(session({
+    secret: "my lil secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
 
 //Creation of Schema and Model to add to this database
 const userSchema = new mongoose.Schema (
     { username: String,
-        password: String }
+        password: String,
+        googleId: String,
+        secret: String
+    }
         );
 
-const secret = 'secret';
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+//const secret = 'secret';
+
+//userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = mongoose.model('User', userSchema);
 
+//How to initilise the passport usage for our user schema
+passport.use(User.createStrategy());
+
+// add data to cookie
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+
+//read cookie for the data
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    })
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.get('/', function (req, res)
 {
     res.render('home');
 })
 
+app.get("/auth/google",
+    passport.authenticate("google", {scope : ['profile']}, function(){
+
+    })
+)
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect('/secrets');
+    });
 
 app.get('/login', function (req, res)
 {
@@ -54,39 +120,87 @@ app.get('/register', function (req, res)
     res.render('register');
 })
 
-app.post('/register', function(req,res){
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password
-    })
-
-    newUser.save(function(err){
-        if(err){
-            res.render(err);
-        }else{
-            res.render('secrets');
-        }
-    });
-
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect("/");
 })
 
-app.post('/login', function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({username: username}, function (err, foundUser){
+app.get("/secrets", function (req,res){
+    User.find({'secret':{$ne:null}}, function(err, foundUsers){
         if(err){
-            res.render('Error: ' + err);
+            console.log(err);
         }else {
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render('secrets');
-                }else{
-                    res.render('Login Failed');
-                }
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers})
             }
         }
     });
+
 })
+
+app.get("/submit", function(req,res){
+    if(req.isAuthenticated()){
+        res.render('submit');
+    }else {
+        res.redirect('/login')
+    }
+})
+
+app.post('/register', function(req,res){
+
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err)
+        {
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets")
+            })
+        }
+    });
+})
+
+app.post('/login', function(req, res){
+
+    const user = new User({
+    username: req.body.username,
+    password: req.body.password
+
+    });
+
+    req.login(user, function(err){
+       if(err){
+           console.log(err);
+           res.redirect("/login");
+       } else {
+           passport.authenticate("local")(req, res, function(){
+               res.redirect("secrets");
+           });
+       }
+    });
+})
+
+app.post('/submit', function(req, res){
+    const secretsubmitted = req.body.secret;
+
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser) {
+                foundUser.secret = secretsubmitted;
+
+                foundUser.save(function()
+                {
+                    res.redirect('/secrets')
+
+
+                });
+            }
+        }
+    })
+})
+
 
 app.listen(port, () => console.log(`REST app listening on port ${port}!`))
